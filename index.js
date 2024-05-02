966d45818c43f3bd276c1c216be80b8da8791ec5
SpoobyBo0 <lion.wallin@elev.ga.ntig.se>
2024-05-02 21:13:37 +0200

    a

--- index.js
+++ index.js
@@ -1,18 +1,12 @@
 var btn = document.getElementById("bokBtn");
-
 btn.onclick = function (event) {
     event.preventDefault();
-
     var modal = document.getElementById('myModal');
     modal.style.display = "block";
-
     setTimeout(function () {
         window.location.href = "index.html";
     }, 3000);
 }
-
-
-
 function kontakta() {
     location.href = "https://www.youtube.com/watch?v=orBQesFBkXg";
 }
